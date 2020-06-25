658e78bf2ca44b522fdaf4d8bec22236c0b83cf3
bronskih <dima5202@mail.ru>
2020-06-25 10:16:53 +0500

    create func SumConcurrently

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/DABronskikh/go-lesson-6/pkg/transactions"
 	"math/rand"
+	"time"
 )
 
 func main() {
@@ -19,4 +20,14 @@ func main() {
 	task1 := transactions.Sort(transitionsSort)
 	fmt.Println(task1)
 
+
+	transitions := make([]transactions.Transaction, users*transitionsPerUser)
+	for idx := range transitions {
+		transitions[idx].Amount = int64(rand.Intn(99))
+		transitions[idx].Date = time.Date(2020, time.Month(rand.Intn(12)), 1, 0, 0, 0, 0, time.Local)
+	}
+
+	total := transactions.SumConcurrently(transitions)
+	fmt.Println(total)
 }
+
--- pkg/transactions/transactions.go
+++ pkg/transactions/transactions.go
@@ -1,8 +1,56 @@
 package transactions
 
 import (
+	"fmt"
 	"sort"
+	"sync"
+	"time"
 )
+
+type Transaction struct {
+	Amount int64
+	Date   time.Time
+}
+
+func Sum(transitions []Transaction) int64 {
+	sum := int64(0)
+	for _, v := range transitions {
+		sum += v.Amount
+	}
+
+	return sum
+}
+
+func SumConcurrently(transactions []Transaction) int64 {
+	transactionsDateGroup := map[time.Time][]Transaction{}
+
+	for _, v := range transactions {
+		_, ok := transactionsDateGroup[v.Date]
+		if !ok {
+			transactionsDateGroup[v.Date] = []Transaction{v}
+		} else {
+			transactionsDateGroup[v.Date] = append(transactionsDateGroup[v.Date], v)
+		}
+	}
+
+	goroutines := len(transactionsDateGroup)
+	wg := sync.WaitGroup{}
+	wg.Add(goroutines)
+
+	total := int64(0)
+
+	for k, v := range transactionsDateGroup {
+		go func(k time.Time, v []Transaction) {
+			sum := Sum(v)
+			total += sum
+			fmt.Printf("date: %v sum: %v\n", k.Format("2006-01"), sum)
+			wg.Done()
+		}(k, v)
+	}
+
+	wg.Wait()
+	return total
+}
 
 func Sort(transitions []int64) []int64 {
 	sort.SliceStable(transitions, func(i, j int) bool {
